77ea3f7b4995d781e7474c3aa41fd288b7324596
Mohit Reddy <mohitreddy1996@gmail.com>
2016-03-31 03:31:51 +0530

    GET Using Forms.

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -1,6 +1,95 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+/*
+ * a simple static http server
+*/
 public class MainServer {
-    public static void main(String args[])
+
+    public static void main(String[] args) throws Exception
     {
-        System.out.println("Mohit Qureto Software Engineering Labs");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8117), 0);
+        server.createContext("/Home", new Home());
+        server.createContext("/UserName",new UserName());
+        server.setExecutor(null);
+        server.start();
     }
+    static class UserName implements HttpHandler
+    {
+        public void handle(HttpExchange data) throws IOException
+        {
+            Map<String,String> M = queryMap(data.getRequestURI().getQuery());
+            String Name = "Hi " + M.get("userName") + "<br> ";
+
+            Headers responseHeaders = data.getResponseHeaders();
+            responseHeaders.set("Content-Type","text/html");
+            BufferedInputStream Stream = new BufferedInputStream(new FileInputStream("/home/mohit/IdeaProjects/QuickChat/src/test/Chat.html"));
+            int StreamLen = Stream.available();
+            String s=" <form name=\"hidform\"> <input type=\"hidden\" name=\"userName\" value='"+M.get("userName")+"'> </form> ";
+            byte[] ByteStream = new byte[StreamLen];
+            Stream.read(ByteStream,0,StreamLen);
+            OutputStream OutStream = data.getResponseBody();
+            data.sendResponseHeaders(200,StreamLen + Name.length() + s.length());
+
+            OutStream.write(ByteStream);
+            OutStream.write(Name.getBytes());
+            OutStream.write(s.getBytes());
+            OutStream.close();
+        }
+    }
+    public static void ViewHome(HttpExchange data) throws IOException
+    {
+
+        Headers responseHeaders = data.getResponseHeaders();
+        responseHeaders.set("Content-Type","text/html");
+        BufferedInputStream Stream = new BufferedInputStream(new FileInputStream("/home/mohit/IdeaProjects/QuickChat/src/test/Home.html"));
+        int StreamLen = Stream.available();
+        String s=" <form name=\"hidform\"> <input type=\"hidden\" name=\"userName\" value='"+"mohit"+"'> </form> ";
+        byte[] ByteStream = new byte[StreamLen];
+        Stream.read(ByteStream,0,StreamLen);
+        OutputStream OutStream = data.getResponseBody();
+        data.sendResponseHeaders(200,StreamLen+s.length());
+        OutStream.write(ByteStream);
+        OutStream.write(s.getBytes());
+        OutStream.close();
+
+    }
+
+    static class Home implements HttpHandler
+    {
+        public void handle(HttpExchange data) throws IOException
+        {
+            ViewHome(data);
+        }
+    }
+
+    public static Map<String, String> queryMap(String query){
+        Map<String, String> result = new HashMap<String, String>();
+        System.out.print("Hurray");
+        for (String param : query.split("&")) {
+            String pair[] = param.split("=");
+            if (pair.length>1) {
+                result.put(pair[0], pair[1]);
+            }else{
+                result.put(pair[0], "");
+            }
+        }
+        return result;
+    }
+
+
+
+
 }
